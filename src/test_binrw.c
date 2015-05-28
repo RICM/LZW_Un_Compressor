e6474eb0333823ec9c5a8d9c2fa6585bea17444b
mnogueron <matthieu.nogueron@live.fr>
2015-05-28 23:30:13 +0200

    start implementation of writeBin

--- src/test_binrw.c
+++ src/test_binrw.c
@@ -5,10 +5,11 @@
 
 int main (int argc, char **argv){
 
-	FILE *file;
+	FILE *file, *file2;
 	file = fopen(argv[1], "rb");
+    file2 = fopen(argv[2], "wb");
 
-	if(!file){
+	if(!file || !file2){
         fprintf(stderr, "Error : file is inaccessible.\n");
         return 1;
     }
@@ -21,7 +22,14 @@ int main (int argc, char **argv){
             tmp = readBin(file, 10);
             displayBinary(tmp);
             printf("\n");
-        }      
+        }
+
+        writeBin(file2, 278, 9, 0); 
+        writeBin(file2, 305, 9, 0); 
+        writeBin(file2, 5000, 16, 0);  
+
+        fclose(file);
+        fclose(file2);
     }
     return 0;
 }
