a281de6d7026cabe155188ead8eb35efae7864d3
Vial-Grelier Aymeric <aymvial@gmail.com>
2015-05-29 00:19:37 +0200

    fixes to add tree element
    Signed-off-by: Vial-Grelier Aymeric <aymvial@gmail.com>

--- src/dictionary.c
+++ src/dictionary.c
@@ -79,30 +79,25 @@ pTree isPresentEncode(pSequence seq, pTree dic[]){
 
 /* Function used to insert an entity in a tree */
 pTree insertInTree(pTree treeToInsert, uint8_t toInsert){
-	printf("to insert vaut : %d\n", toInsert);
 	int increment = 259;
 	pTree treeToReturn = treeToInsert;
 	if (treeToReturn == NULL)
 		printf("first if\n");
 		treeToReturn = createTree(toInsert, increment, NULL, NULL); // to replace nextCode
 		increment++;
+		//return treeToReturn;
 	while(treeToReturn != NULL && treeToReturn->ascii < toInsert){
-		printf("while insertInTree\n");
 		treeToReturn = treeToReturn->left;
 	}
 	if(treeToReturn == NULL){
 		pTree newTree = createTree(toInsert, increment, NULL, NULL);
 		increment++;
 		treeToReturn->left = newTree;
-		printf("new Tree element : %d | %d\n",treeToReturn->ascii, treeToReturn->code );
-		printf("insert sucess\n");
 		return newTree;
 	}else{
 		pTree newTree = createTree(toInsert, increment, treeToReturn->left, NULL);
 		increment++;
 		treeToReturn->left = newTree;
-		printf("new Tree element : %d | %d\n",treeToReturn->ascii, treeToReturn->code );
-		printf("insert sucess\n");
 		return newTree;
 	}
 }
@@ -120,28 +115,20 @@ pTree insertSeqTree(pSequence seq, pTree dic[]){
 	seq = seq->succ;
 	int ok = 1;
 	while (ok == 1){
-		printf("boucle 1\n");
 		save = toTest;
 		uint8_t seqAscii = seq->elem;
 		while(toTest != NULL && toTest->ascii < seqAscii){
-			printf("boucle 2\n");
-			printf("%d\n", toTest->ascii);
 			toTest = toTest->left;
 		}
-		printf("seqAscii vaut %d\n", seqAscii);
-		printf("toTest->ascii vaut %d\n", toTest->ascii);
 		if (toTest->ascii == seqAscii && seq->succ != NULL){
-			printf("if 1\n");
 			seq = seq->succ;
 			if (toTest->right == NULL){
-				printf("if 2\n");
 				seqAscii = seq->elem;
 				toTest = insertInTree(save, seqAscii);
 				return toTest;
 			}
 			toTest = toTest->right;
 		}else{
-			printf("else\n");
 			toTest = insertInTree(save, seqAscii);
 			ok = 0;
 		}
