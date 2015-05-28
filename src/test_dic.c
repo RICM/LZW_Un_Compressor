718ca2cddce3f4e46522433a6caa21fbc4984cc9
Vial-Grelier Aymeric <aymvial@gmail.com>
2015-05-28 17:21:24 +0200

    adapted compress to tree
    Signed-off-by: Vial-Grelier Aymeric <aymvial@gmail.com>

--- src/test_dic.c
+++ src/test_dic.c
@@ -7,13 +7,13 @@ int main (int argc, char **argv){
 	pTree elem6 = createTree(7, 7, NULL, NULL);
 	pTree elem5 = createTree(10, 10, NULL, elem6);
 	pTree elem4 = createTree(4, 4, elem5, NULL);
-	pTree elem1 = createTree(3, 3, NULL, elem4);
-	pTree elem2 = createTree(2, 2, elem1, NULL);
+	pTree elem1 = createTree(3, 3, NULL, NULL);
+	pTree elem2 = createTree(2, 2, elem1, elem4);
 	pTree elem3 = createTree(1, 1, elem2, NULL);
 	Dictionary[1] = elem3;
 	pSequence seq = NULL;
 	uint8_t el = 1;
-	uint8_t el2 = 3;
+	uint8_t el2 = 2;
 	uint8_t el3 = 10;
 	uint8_t el4 = 7;
 	pSequence sequence = add_to_tail(seq, el);
@@ -23,12 +23,11 @@ int main (int argc, char **argv){
 	print_sequence(sequence);
 	if (sequence == NULL)
 		printf("Seq is null\n");
-	printf("ok\n");
 	pTree test = isPresentEncode(sequence, Dictionary);
 	if (test != NULL){
-		printf("printing tree\n");
+		printf("Printing tree\n");
 		print_tree(test);
 	}
 	else
-		printf("goodbye\n");
+		printf("Not Found Bitch !\n");
 }
