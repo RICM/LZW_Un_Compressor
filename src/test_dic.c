10c362d234f476a2594274be0f177597586aa518
Vial-Grelier Aymeric <aymvial@gmail.com>
2015-05-28 16:16:05 +0200

    dictionnary research implemented working
    Signed-off-by: Vial-Grelier Aymeric <aymvial@gmail.com>

--- src/test_dic.c
+++ src/test_dic.c
@@ -4,15 +4,22 @@
 #include "dictionary.h"
 
 int main (int argc, char **argv){
-	pTree elem1 = createTree(1, 1, NULL, NULL);
+	pTree elem6 = createTree(7, 7, NULL, NULL);
+	pTree elem5 = createTree(10, 10, NULL, elem6);
+	pTree elem4 = createTree(4, 4, elem5, NULL);
+	pTree elem1 = createTree(3, 3, NULL, elem4);
 	pTree elem2 = createTree(2, 2, elem1, NULL);
-	pTree elem3 = createTree(3, 3, elem2, NULL);
+	pTree elem3 = createTree(1, 1, elem2, NULL);
 	Dictionary[1] = elem3;
 	pSequence seq = NULL;
 	uint8_t el = 1;
 	uint8_t el2 = 3;
+	uint8_t el3 = 10;
+	uint8_t el4 = 7;
 	pSequence sequence = add_to_tail(seq, el);
 	sequence = add_to_tail(sequence, el2);
+	sequence = add_to_tail(sequence, el3);
+	sequence = add_to_tail(sequence, el4);
 	print_sequence(sequence);
 	if (sequence == NULL)
 		printf("Seq is null\n");
